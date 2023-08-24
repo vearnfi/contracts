f9ee66a708add245101b3bf24d4b5ecae9455a20
fede-rodes <federodes@gmail.com>
2023-08-24 14:47:46 +1200

    cleanup

--- test/Trader.test.ts
+++ test/Trader.test.ts
@@ -27,159 +27,50 @@ describe('Trader', function () {
     const [god, owner, admin, alice, bob] = await getSigners()
 
     const energy = new Contract(ENERGY_CONTRACT_ADDRESS, energyArtifact.abi, god)
-    console.log({ energy: energy.address })
-
-    // const code1 = await ethers.provider.getCode(energy.address)
-    // console.log({code1 })
 
     const VVET9 = await getContractFactory('VVET9', god)
     const vvet9 = await VVET9.deploy()
     const vvet9Abi = VVET9.interface.format(FormatTypes.json) as string
-    console.log({ vvet9: vvet9.address })
-
-    // const code1 = await ethers.provider.getCode(energy.address)
-    // console.log({code1 })
 
     const Factory = await getContractFactory('UniswapV2Factory', god)
     const factory = await Factory.deploy(god.address, vvet9.address)
-    // const Factory = new ContractFactory(factoryArtifact.abi, factoryArtifact.bytecode, god);
-    // const factory = await Factory.deploy(god.address, vvet9.address);
-    // const factory = await Factory.deploy(god.address);
-    console.log({ factory: factory.address })
-
-    // const amount = parseUnits('100', 18)
-    // const tx = await vvet9.connect(god).deposit({ value: amount })
-    // await tx.wait()
-    // const vvet9Balance = await vvet9.balanceOf(god.address)
-    // console.log('VVET done', {vvet9Balance})
-
-    console.log(energy.address, vvet9.address)
 
     const tx1 = await factory.createPair(energy.address, vvet9.address)
-    const receipt = await tx1.wait()
-    // // console.log("TX1 DONE", tx1, receipt);
+    await tx1.wait()
 
     const pairAddress = await factory.getPair(energy.address, vvet9.address)
-    console.log({ pairAddress })
 
     const pair = new Contract(pairAddress, pairArtifact.abi, god)
 
     const reserves = await pair.getReserves()
-    console.log({ reserves })
-    expect(reserves[0].toNumber()).to.equal(0);
-    expect(reserves[1].toNumber()).to.equal(0);
+    expect(reserves[0]).to.equal(0)
+    expect(reserves[1]).to.equal(0)
 
-    // const Router = new ContractFactory(routerArtifact.abi, routerArtifact.bytecode, god);
-    // const router = await Router.deploy(factory.address, vvet9.address);
     const Router = await getContractFactory('UniswapV2Router02', god)
     const router = await Router.deploy(factory.address, vvet9.address)
-    // const Factory = new ContractFactory(factoryArtifact.abi, factoryArtifact.bytecode, god);
-    // const factory = await Factory.deploy(god.address, vvet9.address);
-    // const factory = await Factory.deploy(god.address);
-    console.log({ router: router.address })
 
-    // const code2 = await ethers.provider.getCode(router.address)
-    // console.log({code2 })
-    //==========================
-    // const USDC = await getContractFactory('USDC', god)
-    // const usdc = await USDC.deploy()
-    // const USDT = await getContractFactory('USDT', god)
-    // const usdt = await USDT.deploy()
-    // console.log("DEPLOYS", usdc.address, usdt.address)
-
-    // const tokenAAmount = parseUnits('100', 18) // usdc
-    // const tokenBAmount = parseUnits('100', 18) // usdt
-    // console.log({tokenAAmount, tokenBAmount})
-
-    // const usdcTx = await usdc.mint(god.address, tokenAAmount)
-    // await usdcTx.wait()
-    // const usdtTx = await usdt.mint(god.address, tokenBAmount)
-    // await usdtTx.wait()
-    // console.log("MINTS")
-
-    // const balanceUsdc = await usdc.balanceOf(god.address)
-    // const balanceUsdt = await usdt.balanceOf(god.address)
-    // console.log("BALANCES", {balanceUsdc, balanceUsdt})
-
-    // const approvalT1 = await usdc.connect(god).approve(router.address, constants.MaxUint256)
-    // await approvalT1.wait()
-    // const approvalT2 = await usdt.connect(god).approve(router.address, constants.MaxUint256)
-    // await approvalT2.wait()
-    // console.log("APPROVALS")
-
-    // const allowanceUsdc = await usdc.allowance(god.address, router.address)
-    // const allowanceUsdt = await usdt.allowance(god.address, router.address)
-    // console.log("ALLOWANCE", {allowanceUsdc, allowanceUsdt})
-    // // Rate 1 VVET - 20 VTHO
-
-    // const tx1 = await factory.createPair(usdc.address, usdt.address)
-    // const receipt = await tx1.wait()
-    // // console.log("TX1 DONE", tx1, receipt);
-
-    // const pairAddress = await factory.getPair(usdc.address, usdt.address)
-    // console.log({ pairAddress })
-
-    // const pair = new Contract(pairAddress, pairArtifact.abi, god)
-    // // const code2 = await ethers.provider.getCode(pair.address)
-    // // console.log({code2 })
-
-    // // TODO: check addLiquidityETH. Does it refer to VVET?
-    // const addLiquidityTxTest = await router
-    //   .connect(god)
-    //   .addLiquidity(
-    //     usdc.address,
-    //     usdt.address,
-    //     tokenAAmount,
-    //     tokenBAmount, // amountTokenDesired
-    //     0, // amountTokenMin
-    //     0, // amountETHMin,
-    //     god.address, // to
-    //     constants.MaxUint256, // deadline
-    //     {gasLimit: hexlify(9999999)}
-    //   )
-    //   console.log({addLiquidityTxTest})
-    //   const rec = await addLiquidityTxTest.wait()
-    //   console.log("test done", rec)
-    //==========================
-
-    // const approval1 = await vvet9.connect(god).approve(router.address, constants.MaxUint256)
-    // await approval1.wait()
-    const approval2 = await energy.connect(god).approve(router.address, constants.MaxUint256)
-    await approval2.wait()
-
-    console.log('APPROVALS IN')
-
-    // const WETH = await router.WETH()
-    // console.log({WETH})
-    // const factory_ = await router.factory()
-    // console.log({factory_})
+    const approval = await energy.connect(god).approve(router.address, constants.MaxUint256)
+    await approval.wait()
 
     // Rate 1 VVET - 20 VTHO
-    const token0Amount = parseUnits('1', 18) // vvet
-    const token1Amount = parseUnits('20', 18) // energy
+    const token0Amount = parseUnits('2000', 18) // energy
+    const token1Amount = parseUnits('100', 18) // vvet
 
-    // TODO: check addLiquidityETH. Does it refer to VVET?
-    const addLiquidityTx = await router
-      .connect(god)
-      .addLiquidityETH(
-        // vvet9.address,
-        energy.address, // token
-        // token0Amount,
-        token1Amount, // amountTokenDesired
-        0, // amountTokenMin
-        0, // amountETHMin,
-        // 0,
-        // 0,
-        god.address, // to
-        constants.MaxUint256, // deadline
-      { value: token0Amount, gasLimit: hexlify(9999999) })
-      console.log({addLiquidityTx})
+    const addLiquidityTx = await router.connect(god).addLiquidityETH(
+      energy.address, // token
+      token0Amount, // amountTokenDesired
+      0, // amountTokenMin
+      0, // amountETHMin,
+      god.address, // to
+      constants.MaxUint256, // deadline
+      { value: token1Amount, gasLimit: hexlify(9999999) },
+    )
 
-    const receiptL = await addLiquidityTx.wait()
-    console.log("ADD LIQUIDITY", {receiptL})
+    await addLiquidityTx.wait()
 
     const reserves2 = await pair.getReserves()
-    console.log({ reserves2 })
+    expect(reserves2[0]).to.equal(token0Amount)
+    expect(reserves2[1]).to.equal(token1Amount)
 
     return { energy, god, owner, admin, alice, bob, vvet9Abi }
   }
