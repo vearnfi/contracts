6582e856360df0e139ca032c4d10d2d74ab979c3
fede-rodes <federodes@gmail.com>
2024-04-15 13:25:22 +1200

    adding fetchBaseGasPrice tests

---
+++ test/Trader.fetchBaseGasPrice.test.ts
@@ -0,0 +1,25 @@
+import { expect } from 'chai'
+import { fixture } from './shared/fixture'
+
+describe('Trader.fetchBaseGasPrice', function () {
+  it('should be possible for the anyone to call fetchBaseGasPrice', async function () {
+    // Arrange
+    const { baseGasPrice, trader, alice } = await fixture()
+
+    // Act
+    const tx = await trader.connect(alice).fetchBaseGasPrice()
+    await tx.wait(1)
+    const gasPrice = await trader.baseGasPrice()
+
+    // Assert
+    expect(gasPrice).to.equal(baseGasPrice)
+  })
+
+  it('should emit an event on fetch', async function () {
+    // Arrange
+    const { baseGasPrice, trader, alice } = await fixture()
+
+    // Act + assert
+    await expect(trader.connect(alice).fetchBaseGasPrice()).to.emit(trader, 'FetchGas').withArgs(baseGasPrice)
+  })
+})
